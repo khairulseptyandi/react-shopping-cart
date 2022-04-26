56e7ab6ea2d7db749967ee9147c00dd98ffb94f8
septyandi viki <septyandi.khairul@prosia.co.id>
2022-04-26 16:01:09 +0700

    checkout form

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: ""
     }
+  }
+
+  createOrder = (order) => {
+     alert("Need to save order for : " + order.name)
   }
 
   removeFromCart = (product) => {
@@ -23,7 +27,7 @@ class App extends React.Component {
     this.setState({
       cartItems: cartItems.filter(x => x._id !== product._id),
     })
-    
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x => x._id !== product._id)))
   }
 
   addToCart = (product) => {
@@ -39,6 +43,7 @@ class App extends React.Component {
       cartItems.push({ ...product, count: 1 })
     }
     this.setState({cartItems})
+    localStorage.setItem("cartItems", JSON.stringify(cartItems))
   }
 
   sortProducts = (event) => {
@@ -94,7 +99,8 @@ class App extends React.Component {
             </div>
             <div className="sidebar">
               <Cart cartItems={this.state.cartItems}
-              removeFromCart={this.removeFromCart}/>
+              removeFromCart={this.removeFromCart} 
+              createOrder={this.createOrder}/>
             </div>
           </div>
         </main>
